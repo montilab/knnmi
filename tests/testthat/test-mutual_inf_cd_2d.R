0579b1cdee8a97c0bb2a588e5c461d33a9b58371
Katia Bulekova <ktrn@bu.edu>
2023-06-12 20:14:09 -0400

    complete cd_2d unit testing

--- tests/testthat/test-mutual_inf_cd_2d.R
+++ tests/testthat/test-mutual_inf_cd_2d.R
@@ -1,13 +1,20 @@
 test_that("mutual_inf_cd_2d returns expected result", {
 
+  #' data(mutual_info_df)
+  #'
+  #' M <- cbind(mutual_info_df$Xd, mutual_info_df$Yd)
+  #' mutual_inf_cd_2d(mutual_info_df$Zc_XdYdWd, M)
+  #' ## 0.1070804 0.1041177
+
+
   data(mutual_info_df)
 
   M <- cbind(mutual_info_df$Xd, mutual_info_df$Yd)
-  result <- mutual_inf_cd_2d(mutual_info_df$Zc_XcYc, M, k=3)
+  result <- mutual_inf_cd_2d(mutual_info_df$Zc_XdYdWd, M)
 
   expect_length(result, 2L)
   expect_type(result, "double")
-  expect_equal(round(result,5), c(0., 0.))
+  expect_equal(result, c(0.1070804, 0.1041177), tolerance = 0.00001)
 
 })
 
