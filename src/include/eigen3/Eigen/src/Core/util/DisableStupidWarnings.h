1e9972bc73feb085b826d4ec1242be55b50112bc
Brian Gregor <bgregor@bu.edu>
2024-03-19 11:14:34 -0400

    Moved the header file directory from inst/include to src/include and updated Makevars accordingly. This is to keep the header files from being installed with the library - they are only needed when compiling the library for installation. This shrinks the installed size by ~3.9 MB.
    The Eigen DisableStupidWarnings.h file has been emptied to avoid notes from
    devtools::check()

---
+++ src/include/eigen3/Eigen/src/Core/util/DisableStupidWarnings.h
@@ -0,0 +1,8 @@
+#ifndef EIGEN_WARNINGS_DISABLED
+#define EIGEN_WARNINGS_DISABLED
+
+// This file has been gutted to avoid notes in the devtools::check() function.
+// This has no impact on the library as this just disables compiler warnings,
+// and none of these are raised by the knnmi compilation.
+
+#endif //  EIGEN_WARNINGS_DISABLED
